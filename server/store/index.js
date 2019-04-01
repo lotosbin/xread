83effdafd55e655de86db8ad14109f62e383f0d7
lotosbin <lotosbin@gmail.com>
2019-04-01 12:35:55 +0800

    fix:

--- server/store/index.js
+++ server/store/index.js
@@ -1,10 +1,8 @@
-import {createApolloFetch} from "apollo-fetch";
 import {makeRemoteExecutableSchema, introspectSchema, transformSchema, RenameTypes, RenameRootFields, FilterRootFields} from "graphql-tools"
 import {HttpLink} from 'apollo-link-http';
 
 let uri = process.env.STORE_API_GRAPHQL_URL;
 console.log(`STORE_API_GRAPHQL_URL=${uri}`);
-const fetcher = createApolloFetch({uri: uri});
 
 
 export const createStoreSchema = async () => {
@@ -15,10 +13,6 @@ export const createStoreSchema = async () => {
         schema: introspectionResult,
         link: new HttpLink({uri: uri}),
     });
-    // const schema = makeRemoteExecutableSchema({
-    //     schema: await introspectSchema(fetcher),
-    //     fetcher,
-    // });
     return transformSchema(schema, [
         new FilterRootFields((operation: string, rootField: string) => rootField === "feeds" || rootField === "node"),
         new RenameTypes((name: string) => `Store${name}`),
