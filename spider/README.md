ca0f684d17f8134f153e32e9f0521daa6c9f4d77
lotosbin <lotosbin@gmail.com>
2019-04-24 10:22:11 +0800

    refactor: 分离spider

--- spider/README.md
+++
@@ -1,8 +0,0 @@
-# ENV
-```bash
-set API_URL=http://localhost:4000/graphql
-```
-# RSS 聚合服务
-
-http://www.rssmix.com/
-
--- spider/apollo/client.js
+++
@@ -1,6 +0,0 @@
-import ApolloClient from "apollo-boost";
-
-const client = new ApolloClient({
-    uri: process.env.API_URL
-});
-export default client;
