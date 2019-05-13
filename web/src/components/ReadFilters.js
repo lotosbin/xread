90b27a366a94a5b9f6ab72bcf59e9dea09cd811a
lotosbin <lotosbin@gmail.com>
2019-05-14 00:32:39 +0800

    fix: read filter

--- web/src/components/ReadFilters.js
+++ web/src/components/ReadFilters.js
@@ -4,13 +4,18 @@ import React, {Fragment} from "react";
 import Button from "@material-ui/core/Button";
 import {Link, withRouter} from "react-router-dom";
 
+function query_set(key: string, value: any, search: URLSearchParams) {
+    search.set(key, value);
+    return search;
+}
 const ReadFilters = ({location: {search}}) => {
     let {read = "all"} = queryString.parse(search);
+    let searchParams = new URLSearchParams(search);
     const {t} = useTranslation("", {useSuspense: false});
     return <Fragment>
-        <Button color={read === "all" ? "primary" : "default"} component={Link} to={`?read=all`}>{t('All')}</Button>
-        <Button color={read === "unread" ? "primary" : "default"} component={Link} to={`?read=unread`}>{t('Unread Only')}</Button>
-        <Button color={read === "readed" ? "primary" : "default"} component={Link} to={`?read=readed`}>{t('Read')}</Button>
+        <Button color={read === "all" ? "primary" : "default"} component={Link} to={`?${query_set('read', 'all', searchParams)}`}>{t('All')}</Button>
+        <Button color={read === "unread" ? "primary" : "default"} component={Link} to={`?${query_set('read', 'unread', searchParams)}`}>{t('Unread Only')}</Button>
+        <Button color={read === "readed" ? "primary" : "default"} component={Link} to={`?${query_set('read', 'readed', searchParams)}`}>{t('Read')}</Button>
     </Fragment>
 };
 export default withRouter(ReadFilters);
