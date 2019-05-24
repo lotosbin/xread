280871d8142e688d75c2832dac1910228ddbd572
lotosbin <lotosbin@gmail.com>
2019-05-24 09:10:55 +0800

    fix:

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const typeDefs = gql`${fs.readFileSync(__dirname.concat('/schema.graphql'), 'utf
     server.listen({port: 4000}).then(({url}) => {
         console.log(`🚀  Server ready at ${url}`);
     });
-    // setInterval(runParseArticlePriority, 5000);
-    // setInterval(runParseArticleKeywords, 5000);
-    // setInterval(runParseArticleTopic, 5000);
+    setInterval(runParseArticlePriority, 5000);
+    setInterval(runParseArticleKeywords, 5000);
+    setInterval(runParseArticleTopic, 5000);
 })();
